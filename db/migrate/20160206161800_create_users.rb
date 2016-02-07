e41f9e414a0e00feb04db025a93d44e52cd538fb
Arun <arun.n@railsfactory.org>
2016-02-07 15:00:33 +0530

    added migration

---
+++ db/migrate/20160206161800_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email_id
+      t.string :password_salt
+      t.timestamps
+    end
+  end
+end
