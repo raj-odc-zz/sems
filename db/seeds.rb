4972112e2e82cd2c08db71ba2227d8a9f96061d7
raj-odc <ksraj_odc@yahoo.com>
2016-05-01 12:57:12 +0530

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,12 @@ role =Role.find_or_create_by({name: "admin"})
 board_type = BoardType.find_or_create_by(:name => "School")
 board = Board.find_or_create_by(:name => "school1", :board_type => board_type)
 #class_list=ClassList.create(board_id: board.id,class_name: "test",section: "A",major:"CSE")
-profile_type = ProfileType.find_or_create_by(:name => "Student")
+profile_type = ProfileType.find_or_create_by(:name => "student")
 AddressType.create({:name => "primary"})
 AddressType.create({:name => "secondary"})
 #subjects.each {|sub| Subject.create(:class_list_id => class_list.id,name: sub)}
 #exam_types.each {|name| ExamType.create(:name => name, :class_list_id => class_list.id) }
-User.create({username: "admin",:email => "admin@gmail.com",
+@user = User.create({username: "admin",:email => "admin@gmail.com",
                  :role_id => role.id, :password => "password",
                  :password_confirmation => "password", :profile_type => profile_type })
+@profile = @user.build_profile(:profile_type_id => profile_type, :board_id => board.id).save
