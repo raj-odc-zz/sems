df9b6681a5a45d61e6abb88211ecf272f3b7a6c8
Arun <arunkumarit02@gmail.com>
2016-05-01 10:36:35 +0530

    changes for the merge confilit

--- public/assets/api/work_experiences-5151c8abdc608976164294d0c1ffa2ae3a2f540252a1f785784971afaf2fd246.js
+++
@@ -1,1 +0,0 @@
-function submitForm(e){var t=$(e),i=t.serialize(),s=t.attr("action"),n=t.attr("method");formAjaxCall(s,n,i)}function formAjaxCall(e,t,i){$.ajax({url:e,type:t,data:i,success:function(e){window.location.href="/profiles"}})}
